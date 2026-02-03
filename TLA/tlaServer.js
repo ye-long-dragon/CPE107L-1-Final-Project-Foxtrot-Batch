1380f03a619d866dcba9776037893295b6dac393
KuraidoXenpaI <asdwera996@gmail.com>
2026-02-03 15:55:21 +0800

    changed all .html to .ejs \n changed the dashboard to weekly layout.

--- TLA/tlaServer.js
+++ TLA/tlaServer.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const path = require('path');
+// package.json must include: "type": "module"
+
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname replacement in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 8400;
 
 // Middleware
 app.use(express.json());
@@ -14,24 +21,26 @@ app.use('/views', express.static(path.join(__dirname, 'views')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+    res.render(path.join(__dirname, 'views', 'dashboard.ejs'));
 });
 
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+    res.render(path.join(__dirname, 'views', 'dashboard.ejs'));
 });
 
 app.get('/form', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'form.html'));
+    res.render(path.join(__dirname, 'views', 'form.ejs'));
 });
 
 app.get('/overview', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'overview.html'));
+    res.render(path.join(__dirname, 'views', 'overview.ejs'));
 });
 
 // Start server
 app.listen(PORT, () => {
+    console.log("Current working directory:", process.cwd());
     console.log(`TLA Server running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+// Export app (optional)
+export default app;
