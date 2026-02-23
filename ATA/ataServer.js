91896f5641d88535c44cb02d60f26b35c8f473d4
Duff Bastasa <drainbastasa1256@gmail.com>
2026-02-23 22:48:01 +0800

    Fixed server port.

--- ATA/ataServer.js
+++ ATA/ataServer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.ATAPORT;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
