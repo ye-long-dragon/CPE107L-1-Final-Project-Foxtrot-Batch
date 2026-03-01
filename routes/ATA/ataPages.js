98527df8a011ff18f8ba079d166345df020b9eeb
Vince Lawrence Baraclan <bvincelawrence@gmail.com>
2026-03-01 16:22:07 +0800

    Updated ATA Views

--- routes/ATA/ataPages.js
+++ routes/ATA/ataPages.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => res.render("ATA/AtaMain")); // NO leading slash, m
 router.get("/new", (req, res) => res.render("ATA/new-ata"));
 
 // Dashboard window
-router.get("/dashboardwindow", (req, res) => res.render("ATA/dashboard_window"));
+router.get("/dashboard/window", (req, res) => res.render("ATA/dashboard_window"));
 
 // Other pages
 router.get("/submissions", (req, res) => res.render("ATA/submissions"));
